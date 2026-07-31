e7a8ded6e648397f3ca40abea7c968727e50d85b
apistech <apistech@gmail.com>
2026-07-31 12:59:01 +0700

    Update generate_liveevents.py

--- generate_liveevents.py
+++ generate_liveevents.py
@@ -26,14 +26,13 @@ OUTPUT_DIR = Path("playlists")
 
 DEFAULT_HEADERS = {
     "User-Agent": (
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 "
-        "(KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36"
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/150.0.0.0 Safari/537.36 Edg/150.0.0.0"
     )
 }
 
 # Add your M3U URLs here
 SOURCES = [
-    "https://github.com/doms9/iptv/raw/refs/heads/default/M3U8/events.m3u8",
+    "https://github.com/BuddyChewChew/sports/raw/refs/heads/main/liveeventsfilter.m3u8",
 ]
 
 # Stream content types (whitelist)
